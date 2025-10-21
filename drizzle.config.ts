8b55b53b324ad279a1d58c731cf082509b2c66ef
Logan McAnsh <logan@mcan.sh>
2025-10-21 11:46:37 -0400

    chore: typesafe drizzle connection

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,4 +1,22 @@
 import { defineConfig } from "drizzle-kit"
+import * as z from "zod/mini"
+
+let cloudflare = z.pipe(
+	z.object({
+		CLOUDFLARE_ACCOUNT_ID: z.string(),
+		CLOUDFLARE_DATABASE_ID: z.string(),
+		CLOUDFLARE_TOKEN: z.string(),
+	}),
+	z.transform((input) => {
+		return {
+			ACCOUNT_ID: input.CLOUDFLARE_ACCOUNT_ID,
+			DATABASE_ID: input.CLOUDFLARE_DATABASE_ID,
+			ACCESS_TOKEN: input.CLOUDFLARE_TOKEN,
+		}
+	}),
+)
+
+let env = cloudflare.parse(process.env)
 
 export default defineConfig({
 	out: "./drizzle",
@@ -6,8 +24,8 @@ export default defineConfig({
 	dialect: "sqlite",
 	driver: "d1-http",
 	dbCredentials: {
-		accountId: process.env.CLOUDFLARE_ACCOUNT_ID,
-		databaseId: process.env.CLOUDFLARE_DATABASE_ID,
-		token: process.env.CLOUDFLARE_TOKEN,
+		accountId: env.ACCOUNT_ID,
+		databaseId: env.DATABASE_ID,
+		token: env.ACCESS_TOKEN,
 	},
 })
